118ec52b03777f2120ff516e7aa8ab20e67f9c4d
rohan igave <rigave200@gmail.com>
2024-02-11 02:53:46 +0530

    trial 1

--- routes/anime.js
+++ routes/anime.js
@@ -5,13 +5,13 @@ const anime = require('../controllers/anime');
 const catchAsync = require('../utils/catchAsync');
 
 router.get('/', catchAsync(anime.index));
-router.post('/anime/search', catchAsync(anime.searchAnime));
-router.get('/aired', catchAsync(anime.airedAnime));
-router.get('/upcoming', catchAsync(anime.upcomingAnime));
-router.get('/favorite', catchAsync(anime.favoriteAnime));
-router.get('/:id', catchAsync(anime.showAnime));
-router.get('/characters/top', catchAsync(anime.topCharacters));
-router.post('/characters', catchAsync(anime.searchCharacter));
-router.get('/:id/characters', catchAsync(anime.getCharacter));
+// router.post('/anime/search', catchAsync(anime.searchAnime));
+// router.get('/aired', catchAsync(anime.airedAnime));
+// router.get('/upcoming', catchAsync(anime.upcomingAnime));
+// router.get('/favorite', catchAsync(anime.favoriteAnime));
+// router.get('/:id', catchAsync(anime.showAnime));
+// router.get('/characters/top', catchAsync(anime.topCharacters));
+// router.post('/characters', catchAsync(anime.searchCharacter));
+// router.get('/:id/characters', catchAsync(anime.getCharacter));
 
 module.exports = router;
